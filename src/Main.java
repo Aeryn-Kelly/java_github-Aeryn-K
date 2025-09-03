fb22651235b9dda3529bd403f3103e58b5e3dee5
AbyssalCitadel <katkel.002@gmail.com>
2025-09-03 14:28:08 -0700

    Fixed Aer's print message to be a print to lane Also asked a very important question

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("[/87><=");
+        System.out.println("[/87><=");
+        System.out.println("Am I that much of an edgelord?");
     }
 }
